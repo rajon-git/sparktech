6283012af93906ffb2104e975ff4b92f9dba7075
rajon-git <rajon.zhsust15@gmail.com>
2025-08-08 00:23:44 +0600

    admin create book, update and delete also apni create

--- api/urls.py
+++ api/urls.py
@@ -11,8 +11,13 @@ urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    # books api
+    # books api for all users
     path('books/<int:id>/', book_views.GetBookViews.as_view(), name='book_detail'),
     path('books/', book_views.GetBookListViews.as_view(), name='all_books'),
+
+    # books api for only admin
+    path('api/books/', book_views.BookCreateView.as_view(), name='book-create'),
+    path('api/books/<int:id>/', book_views.BookUpdateView.as_view(), name='book-update'),
+    path('api/books/<int:id>/', book_views.BookDeleteView.as_view(), name='book-delete'),
 ] 
 
--- books/views.py
+++ books/views.py
@@ -9,6 +9,32 @@ class GetBookViews(generics.RetrieveAPIView):
     lookup_field  = 'id'
 
 class GetBookListViews(generics.ListAPIView):
+    serializer_class = BookSerializer
+    
+    def get_queryset(self):
+        queryset = Book.objects.all()
+        author_id = self.request.query_params.get('author')
+        category_id = self.request.query_params.get('category')
+
+        if author_id:
+            queryset = queryset.filter(author_id=author_id)
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+        return queryset
+    
+class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    # permission_classes = [permissions.IsAdminUser]
+    permission_classes = [permissions.IsAdminUser]
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAdminUser]
+    lookup_field = 'id'
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAdminUser]
+    lookup_field = 'id'
